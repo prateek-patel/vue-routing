d6d4a920e2235d64ac6d54bf65e998feb0908616
Prateek Patel <cwr.prateek.patel@asurion.com>
2021-09-22 17:15:17 +0530

    routing project

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        // for 'redirect' url changes from localhost:8080 to localhost:8080/teams
+        { path: '/', redirect: '/teams' },
+        // for 'alias' url doesn't change it loads as localhost:8080
+        // { path: '/teams', component: TeamsList, alias: '/' },
+        { path: '/teams', component: TeamsList, alias: '/' },
+        { path: '/users', component: UsersList },
+        { path: '/teams/:teamId', component: TeamMembers, props: true },
+        { path: '/:notFound(.*)', component: NotFound }
+    ]
+});
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app');
