a9dcde7055fd86d608ac35d0f15e092276f71e28
Prateek Patel <cwr.prateek.patel@asurion.com>
2021-09-23 10:52:31 +0530

    multiple routes with named routers

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
 import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
@@ -17,12 +19,12 @@ const router = createRouter({
         {
             name: 'teams',
             path: '/teams',
-            component: TeamsList,
+            components: { default: TeamsList, footer: TeamsFooter },
             children: [
                 { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
             ]
         },
-        { path: '/users', component: UsersList },
+        { path: '/users', components: { default: UsersList, footer: UsersFooter } },
         { path: '/:notFound(.*)', component: NotFound }
     ]
 });
